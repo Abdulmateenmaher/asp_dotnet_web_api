94498902b91f2026b98ab4f6894c60bde4642cd6
abdulmateen maher <abdulmateenmaher23@gmail.com>
2025-01-15 19:59:31 +0430

    added project to remote first commit'

--- obj/Debug/net9.0/webApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/webApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75b3da4be5f1bc217d7a067e83cd2e6c8c547a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
